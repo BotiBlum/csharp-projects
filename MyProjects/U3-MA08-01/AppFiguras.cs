173446a347911a7e58bf93d62f2469c1653ab6b0
Pablo Vergaray <almontes@gmail.com>
2023-06-19 22:18:17 -0400

    cambios

---
+++ MyProjects/U3-MA08-01/AppFiguras.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MA08
+{
+    public class AppFiguras
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hola Unidad 03");
+            probandoPuntos();
+        }
+
+
+        public static void probandoPuntos()
+        {
+            Punto punto1 = new Punto(); 
+        }
+    }
+
+}
